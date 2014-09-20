6373e51395a9154d104edd3777b2516e3fe21d8e
Chad Windnagle <drmmr763@gmail.com>
2014-09-20 11:53:04 -0400

    add pre and post text fields to config and output. add no event error check

--- frontend/views/eventbrites/tmpl/default.php
+++ frontend/views/eventbrites/tmpl/default.php
@@ -7,14 +7,30 @@
 
 defined('_JEXEC') or die;
 
+$params = JComponentHelper::getParams('com_eventbrite');
+
+$title = JFactory::getApplication()->getMenu()->getActive()->title;
+
 ?>
 
-<ul>
-    <?php foreach($this->items as $item): ?>
-        <li>
-            <a href="<?php echo JRoute::_('index.php?option=com_eventbrite&view=eventbrite&id=' . $item->id); ?>">
-                <?php echo $item->title; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<h3 class="catItemTitle"><?php echo $title; ?></h3>
+
+<?php echo $params->get('pretext'); ?>
+
+<?php if(count($this->items)): ?>
+    <ul>
+        <?php foreach($this->items as $item): ?>
+            <li>
+                <a href="<?php echo JRoute::_('index.php?option=com_eventbrite&view=eventbrite&id=' . $item->id); ?>">
+                    <?php echo $item->title; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    <div class="alert alert-info">
+        <p>No events found!</p>
+    </div>
+<?php endif; ?>
+
+<?php echo $params->get('posttext'); ?>
